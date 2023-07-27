8b693b182732ab4e012ab06f7b5c41edc8167fde
DESKTOP-UTB7950\USER <songsaemiye@naver.com>
2023-07-27 13:37:18 +0900

    timeline 완료

---
+++ src/main/java/com/sns/timeline/TimelineController.java
@@ -0,0 +1,32 @@
+package com.sns.timeline;
+
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sns.timeline.bo.TimelineBO;
+import com.sns.timeline.domain.CardView;
+
+@RequestMapping("/timeline")
+@Controller
+public class TimelineController {
+
+	@Autowired
+	private TimelineBO timelineBO;
+	
+	@GetMapping("/timeline_view")
+	public String timelineView(Model model, HttpSession session) {
+		Integer userId = (Integer)session.getAttribute("userId");
+		
+		List<CardView> cardList = timelineBO.generateCardViewList(userId);
+		model.addAttribute("cardList", cardList);
+		model.addAttribute("view", "timeline/timeline");
+		return "template/layout";
+	}
+}
---
+++ src/main/java/com/sns/timeline/bo/TimelineBO.java
@@ -0,0 +1,79 @@
+package com.sns.timeline.bo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sns.comment.bo.CommentBO;
+import com.sns.comment.domain.CommentView;
+import com.sns.like.bo.LikeBO;
+import com.sns.post.bo.PostBO;
+import com.sns.post.entity.PostEntity;
+import com.sns.timeline.domain.CardView;
+import com.sns.user.bo.UserBO;
+import com.sns.user.entity.UserEntity;
+
+@Service
+public class TimelineBO {
+
+	@Autowired
+	private PostBO postBO;
+	
+	@Autowired
+	private UserBO userBO;
+	
+	@Autowired
+	private CommentBO commentBO;
+	
+	@Autowired
+	private LikeBO likeBO;
+	// input: X
+	// output: List<CardView>
+	// 비로그인시에도 카드리스트는 뿌려져야하므로 Integre userId - null 허용
+	public List<CardView> generateCardViewList(Integer userId) {
+		List<CardView> cardViewList = new ArrayList<>(); // []
+		
+		// 글 목록 가져온다.
+		List<PostEntity> postList = postBO.getPostList();
+		
+		// 글 목록 반목문 순회
+		// postEntity => cardView   => cardViewList에 담는다.
+		for (PostEntity post : postList) {  // 0 1 2
+			// post에 대응되는 하나의 카드를 만든다.
+			CardView card = new CardView();
+			
+			// 글을 세팅한다.
+			card.setPost(post);
+			
+			// 글쓴이를 세팅한다.
+			UserEntity user = userBO.getUserEntityById(post.getUserId());
+			card.setUser(user);
+			
+			// 댓글들을 세팅한다.
+			List<CommentView> commentViewList = commentBO.generateCommentViewList(post.getId());
+			card.setCommentList(commentViewList);
+			
+			// 좋아요 개수
+			int likeCount = likeBO.getLikeCountByPostId(post.getId());
+			card.setLikeCount(likeCount);
+			
+			// 좋아요 여부
+			boolean filledLike = likeBO.filledLike(post.getId(), userId);
+			card.setFilledLike(false);
+			
+			
+			//★★★★★★ cardViewList에 담는다.
+			cardViewList.add(card);
+		}
+		
+		return cardViewList;
+	}
+	
+	
+}
+
+
+
+
