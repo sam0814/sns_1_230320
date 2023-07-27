7795d425720451993a7481ed6f088360c0a9cd0a
DESKTOP-UTB7950\USER <songsaemiye@naver.com>
2023-07-27 13:35:54 +0900

    user 로그인/회원가입 마크업 및 흐름

---
+++ src/main/java/com/sns/user/UserController.java
@@ -0,0 +1,51 @@
+package com.sns.user;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/user")
+@Controller
+public class UserController {
+
+	/**
+	 * 회원가입 화면
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/sign_up_view")
+	public String signUpView(Model model) {
+		model.addAttribute("view", "user/signUp");
+		return "template/layout";
+	}
+	
+	/**
+	 * 로그인 화면
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/sign_in_view")
+	public String signInView(Model model) {
+		model.addAttribute("view", "user/signIn");
+		return "template/layout";
+	}
+	
+	/**
+	 * 로그아웃
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping("/sign_out")
+	public String signOut(HttpSession session) {
+		// 세션 비우기
+		session.removeAttribute("userId");
+		session.removeAttribute("userName");
+		session.removeAttribute("userLoginId");
+		
+		// 타임라인 화면으로 이동 
+		return "redirect:/timeline/timeline_view";
+	}
+}
---
+++ src/main/java/com/sns/user/bo/UserBO.java
@@ -0,0 +1,47 @@
+package com.sns.user.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sns.user.dao.UserRepository;
+import com.sns.user.entity.UserEntity;
+
+@Service
+public class UserBO {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public UserEntity getUserEntityByLoginId(String loginId) {
+		return userRepository.findByLoginId(loginId);
+	}
+
+	public Integer addUser(String loginId, String password, String name, String email) {
+		// save
+		UserEntity userEntity = userRepository
+				.save(UserEntity.builder()
+						.loginId(loginId)
+						.password(password)
+						.name(name)
+						.email(email)
+						.build());
+		return userEntity == null ? null : userEntity.getId();
+	}
+	
+	public UserEntity getUserEntityByLoginIdPassword(String loginId, String password) {
+		return userRepository.findByLoginIdAndPassword(loginId, password);
+	}
+	
+	public UserEntity getUserEntityById(int userId) {
+		return userRepository.findById(userId).orElse(null);
+	}
+}
+
+
+
+
+
+
+
+
+
